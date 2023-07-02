7ea309e488dd0e51aaff859fd5482a9df6963f1d
ken <ynkn0829@gmail.com>
2023-07-02 17:56:47 +0900

    commit

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 # Create a VPC
-# resource "aws_vpc" "tf_cloud_playroom" {
-#   cidr_block = "10.1.0.0/16"
+resource "aws_vpc" "tf_cloud_playroom" {
+  cidr_block = "10.1.0.0/16"
 
-#   tags = {
-#     Name = "tf_cloud_playroom"
-#   }
-# }
+  tags = {
+    Name = "tf_cloud_playroom"
+  }
+}
